8a9121d9b02d2af855a49328382763377432a08a
Mikhail <119260605+milaxcoo@users.noreply.github.com>
2023-11-22 18:46:42 +0000

    commit

--- test1.py
+++ test1.py
@@ -1,22 +1,38 @@
 class Matrix:
+    #конструктор
     def __init__(self, matrix):
         self.matrix = matrix
 
+    #методы подписаны
     def determinant(self):
         if len(self.matrix) == 1:
             return self.matrix[0][0]
+            #easy way
         else:
             det = 0
             for i in range(len(self.matrix)):
                 det += self.matrix[0][i] * self.cofactor(0, i)
             return det
         
+    #сборка матрицы из миноров
     def cofactor(self, i, j):
-        return (-1) ** (i + j) * Matrix([[self.matrix[m][n] for n in range(len(self.matrix[0])) if n != j] for m in range(len(self.matrix)) if m != i]).determinant()
+        return (-1) ** (i + j) * self.minor(i, j).determinant()
+        
+    def minor(self, i, j):
+        matrix = []
+        for m in range(len(self.matrix)):
+            if m != i:
+                row = []
+                for n in range(len(self.matrix[0])):
+                    if n != j:
+                        row.append(self.matrix[m][n])
+                matrix.append(row)
+        return Matrix(matrix)
     
     def transpose(self):
         return Matrix([[self.matrix[j][i] for j in range(len(self.matrix))] for i in range(len(self.matrix[0]))])
     
+    #вывод
     def show(self):
         for i in range(len(self.matrix)):
             for j in range(len(self.matrix[0])):
