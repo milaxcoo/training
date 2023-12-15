a8b5875846a13341afa329d138ae88f9302fb717
Mikhail <119260605+milaxcoo@users.noreply.github.com>
2023-12-15 14:26:05 +0000

    weqweqwe

---
+++ kramer.py
@@ -0,0 +1,43 @@
+def determinant(matrix):
+    return matrix[0][0] * matrix[1][1] * matrix[2][2] + matrix[0][1] * matrix[1][2] * matrix[2][0] + matrix[0][2] * matrix[1][0] * matrix[2][1] - matrix[0][2] * matrix[1][1] * matrix[2][0] - matrix[0][1] * matrix[1][0] * matrix[2][2] - matrix[0][0] * matrix[1][2] * matrix[2][1]
+
+
+def kramer(matrix, vector):
+    det = determinant(matrix)
+    if det == 0:
+        return "Система не имеет единственного решения (определитель матрицы равен 0)"
+    
+    matrix_x = [
+        [vector[0], matrix[0][1], matrix[0][2]],
+        [vector[1], matrix[1][1], matrix[1][2]],
+        [vector[2], matrix[2][1], matrix[2][2]]
+    ]
+
+    matrix_y = [
+        [matrix[0][0], vector[0], matrix[0][2]],
+        [matrix[1][0], vector[1], matrix[1][2]],
+        [matrix[2][0], vector[2], matrix[2][2]]
+    ]
+
+    matrix_z = [
+        [matrix[0][0], matrix[0][1], vector[0]],
+        [matrix[1][0], matrix[1][1], vector[1]],
+        [matrix[2][0], matrix[2][1], vector[2]]
+    ]
+
+    x = determinant(matrix_x) / det
+    y = determinant(matrix_y) / det
+    z = determinant(matrix_z) / det
+
+    return x, y, z
+
+
+matrix = [
+    [2, -4, 3],
+    [3, -1, 5],
+    [1, -2, 4]
+]
+vector = [1, 2, 3]
+
+solution = kramer(matrix, vector)
+print("Решение системы уравнений:", solution)
--- solvematrix.py
+++ solvematrix.py
@@ -5,10 +5,10 @@ coefficients = np.array([
     [2, -4, 3],
     [3, -1, 5],
     [1, -2, 4]
-])
+]) #А
 
 
-constants = np.array([1, 2, 3])
+constants = np.array([1, 2, 3]) #строчка В
 
 
 solution = np.linalg.solve(coefficients, constants)
